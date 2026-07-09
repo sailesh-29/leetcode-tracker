0940f9ff698bc070da88021f59ff431d91384cf4
sailesh-29 <snehasailesh5@gmail.com>
2026-07-09 10:07:25 +0530

    Create 104-MaximumDepthOfBinaryTree

---
+++ 104-MaximumDepthOfBinaryTree/104-MaximumDepthOfBinaryTree.py
@@ -0,0 +1,25 @@
+# Last updated: 7/9/2026, 10:07:23 AM
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        queue = deque([(root)])
+        max_depth = 0
+
+        while queue:
+            level_size = len(queue)
+            for _ in range(level_size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            max_depth += 1
+
+        return max_depth
