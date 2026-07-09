34598455eee164011bbc36e380120d761b1b1ebc
sailesh-29 <snehasailesh5@gmail.com>
2026-07-09 10:07:04 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -0,0 +1,14 @@
+# Last updated: 7/9/2026, 10:07:02 AM
+class Solution(object):
+    def moveZeroes(self, nums):
+        j = 0
+        for num in nums:
+            if num != 0:
+                nums[j] = num
+                j += 1
+        for i in range(j, len(nums)):
+            nums[i] = 0
+                
+    
+
+        
